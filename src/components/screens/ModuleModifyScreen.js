876b5b2e162bf85644b89913868566c4a2152592
Ashvinderassi <k2271638@kingston.ac.uk>
2024-10-14 22:39:11 +0100

    2nd Video (#1)

---
+++ src/components/screens/ModuleModifyScreen.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Screen from '../layout/Screen';
+
+export const ModuleModifyScreen = () => {
+  // Initialsations -------------------
+  // State ----------------------------
+  // Handlers -------------------------
+  // View -----------------------------
+  return (
+    <Screen>
+
+      <Text>Modify</Text>
+
+    </Screen>
+
+  );
+};
+
+const styles = StyleSheet.create({ });
+
+export default ModuleModifyScreen;
